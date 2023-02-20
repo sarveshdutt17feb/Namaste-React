0529b3e423914dc927bee02f6e527cfa827a8488
Sarvesh Richhariya <sarveshdutt40@gmail.com>
2023-02-20 19:29:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading1  = React.createElement("h1",{
+    id:"little",
+    className:"myClass",
+    
+},"Heading 1 for parcel");
+const heading2  = React.createElement("h2",{
+    id:"little1",
+    className:"myClass",
+    
+},"Heading 2");
+
+const container = React.createElement("div",{
+    id:"container",
+},[heading1,heading2]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
+//this commit is for bundler!!
